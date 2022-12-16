ee3293b5f4ac48c7f69a972a6077a7a433650060
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-12-16 20:54:44 +0100

    Start new course.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1>This is an home  page</h1>
+    </div>
+  </main>
+</template>
+
+<style>
+
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+</style>
